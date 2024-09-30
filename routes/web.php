02887bf10e8c3b8ad0d5e6e03a0f244dc3b2ba4a
AndreaLN25 <andrealn14@gmail.com>
2024-10-01 01:28:00 +0200

    Applying middleware to routes

--- routes/web.php
+++ routes/web.php
@@ -10,13 +10,10 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
-// Rutas de autenticación
-require __DIR__.'/auth.php'; // Incluye las rutas de autenticación generadas por Breeze
-
-// Rutas públicas y protegidas por autenticación (no requieren 'auth:sanctum')
-Route::resource('expenses', ExpenseController::class); // CRUD de gastos
-Route::resource('incomes', IncomeController::class); // CRUD de ingresos
-Route::resource('categories', CategoryController::class); // CRUD de categorías
-
-// Rutas de usuarios (pueden ser públicas o protegidas según tu necesidad)
 Route::resource('users', UserController::class); // CRUD de usuarios
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('expenses', ExpenseController::class); // CRUD de gastos
+    Route::resource('incomes', IncomeController::class); // CRUD de ingresos
+    Route::resource('categories', CategoryController::class); // CRUD de categorías
+});
