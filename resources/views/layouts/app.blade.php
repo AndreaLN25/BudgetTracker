c6ae5c1048bd60ca51c5a24047eac1bac55b5792
AndreaLN25 <andrealn14@gmail.com>
2024-10-02 12:40:08 +0200

    Align logout button to the right and switch to English. Categories and users not showing in the navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,23 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Budget Tracker</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
 </head>
+
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="{{ url('/') }}">Budget Tracker</a>
         <div class="collapse navbar-collapse">
             <ul class="navbar-nav mr-auto">
-                @if(Auth::check())
+                @if (Auth::check())
                     <li class="nav-item"><a class="nav-link" href="{{ route('incomes.index') }}">Incomes</a></li>
                     <li class="nav-item"><a class="nav-link" href="{{ route('expenses.index') }}">Expenses</a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{ route('categories.index') }}">Categories</a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{ route('users.index') }}">Users</a></li>
+                    {{-- <li class="nav-item"><a class="nav-link" href="{{ route('categories.index') }}">Categories</a></li> --}}
+                    {{-- <li class="nav-item"><a class="nav-link" href="{{ route('users.index') }}">Users</a></li> --}}
+                @endif
+            </ul>
+            <ul class="navbar-nav ms-auto">
+                @if (Auth::check())
                     <li class="nav-item">
-                        <button class="nav-link btn btn-link" style="cursor: pointer;" data-bs-toggle="modal" data-bs-target="#logoutModal">Logout</button>
+                        <button class="nav-link btn btn-link" style="cursor: pointer;" data-bs-toggle="modal"
+                            data-bs-target="#logoutModal">Logout</button>
                     </li>
                 @else
                     <li class="nav-item"><a class="nav-link" href="{{ route('login.store') }}">Login</a></li>
@@ -35,18 +42,18 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="logoutModalLabel">Cerrar Sesión</h5>
+                    <h5 class="modal-title" id="logoutModalLabel">Logout</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    ¿Estás seguro de que deseas cerrar sesión?
+                    Are you sure you want to log out?
                 </div>
                 <div class="modal-footer">
                     <form action="{{ route('logout') }}" method="POST" id="logoutForm">
                         @csrf
-                        <button type="submit" class="btn btn-danger">Cerrar Sesión</button>
+                        <button type="submit" class="btn btn-danger">Logout</button>
                     </form>
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
                 </div>
             </div>
         </div>
@@ -54,4 +61,5 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
